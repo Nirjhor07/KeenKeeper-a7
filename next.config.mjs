19ff6f900dd8a8dec3114e2c6afb178b6d2f2a60
Ashraf Hossain Akash <nirjhorakash07@gmail.com>
2026-04-15 13:44:45 +0600

    Image added to each friend card and optimized perfectly

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  // https://images.unsplash.com/photo-1544005313-94ddf0286df2
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- src/app/Components/FriendCard/FriendCard.jsx
+++ src/app/Components/FriendCard/FriendCard.jsx
@@ -1,46 +1,62 @@
+import Image from "next/image";
 
-
-const FriendCard = ({f}) => {
-    console.log(f)
-    const {name,vnext_due_date,bio,goal,status,days_since_contact,email,picture,id, tags}=f;
-    return (
-        <div className="container mx-auto bg-gray-100 ">
-      
+const FriendCard = ({ f }) => {
+  console.log(f);
+  const { name, status, days_since_contact, picture, tags, priority } = f;
+  return (
+    <div className="container mx-auto bg-gray-100 ">
       <div className="bg-white  rounded-xl shadow-md p-6 text-center space-y-3 ">
-        
         {/* Profile Image */}
-        <div className="flex justify-center">
-         
+        <div className="flex justify-center ">
+          <div className="w-24 h-24 rounded-full overflow-hidden">
+            <Image
+              className="w-full h-full object-cover"
+              src={picture + "?w=200&q=80"} // eta korar uddessho hoilo : image  size reduce & quality for faster loading
+              alt={name}
+              width={100}
+              height={100}
+              loading={priority ? "eager" : "lazy"} // eta korsi cz some image was loading slow so i load the image before
+            ></Image>
+          </div>
         </div>
 
         {/* Name */}
-        <h2 className="text-lg font-semibold text-gray-800">
-         {name}
-        </h2>
+        <h2 className="text-lg font-semibold text-gray-800">{name}</h2>
 
         {/* Time */}
         <p className="text-sm text-gray-400">{days_since_contact}d ago</p>
 
         {/* Tag */}
         <div className="flex gap-2 justify-center items-center">
-          {
-            tags.map((tag, index)=><span key={index} tag={tag} className="bg-green-100 text-green-700 text-xs px-3 py-1 rounded-full font-medium">
-            {tag}
-          </span>)
-          }
+          {tags.map((tag, index) => (
+            <span
+              key={index}
+              tag={tag}
+              className="bg-green-100 text-green-700 text-xs px-3 py-1 rounded-full font-medium"
+            >
+              {tag}
+            </span>
+          ))}
         </div>
 
         {/* Status */}
         <div>
-          <span className={`py-1 px-3 rounded-2xl ${status === 'on track' ? 'bg-green-700 text-white ': status === 'overdue' ? 'bg-red-600': 'bg-yellow-300'}`}>
+          <span
+            className={`py-1 px-3 rounded-2xl text-sm
+                 ${
+                   status === "on track"
+                     ? "bg-green-700 text-white "
+                     : status === "overdue"
+                       ? "bg-red-600"
+                       : "bg-yellow-300"
+                 }`}
+          >
             {status}
           </span>
         </div>
-
       </div>
-
     </div>
-    );
+  );
 };
 
 export default FriendCard;
