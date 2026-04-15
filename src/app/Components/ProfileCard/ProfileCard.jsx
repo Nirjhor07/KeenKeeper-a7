6a5384d25625d1146d69620af310fb0730860ec3
Ashraf Hossain Akash <nirjhorakash07@gmail.com>
2026-04-15 19:23:04 +0600

    details card section completted added date image goal next due date used Math.ceil just left the quick check in functionality

--- src/app/Components/ProfileCard/ProfileCard.jsx
+++ src/app/Components/ProfileCard/ProfileCard.jsx
@@ -1,41 +1,56 @@
+import Image from "next/image";
 import { BiArchiveIn } from "react-icons/bi";
-import { FaDeleteLeft } from "react-icons/fa6";
 import { MdDelete, MdSnooze } from "react-icons/md";
 
-const ProfileCard = ({details}) => {
-    console.log(details)
+const ProfileCard = ({ details }) => {
+  // console.log(details)
+  const { days_since_contact, bio, status, name, picture, email } = details;
   return (
     <div>
-      {/* LEFT SIDEBAR */}
+      {/* Left side-bar */}
       <div className="flex flex-col gap-4">
         {/* Profile Card */}
         <div className="card bg-base-100 shadow-sm ">
           <div className="card-body items-center text-center p-6">
             <div className="avatar mb-2">
               <div className="w-16 rounded-full">
-                {/* <img src="https://i.pravatar.cc/150?img=32" /> */}
+                <Image
+                  className="w-full h-full object-cover"
+                  src={picture + "?w=200&q=80"} // eta korar uddessho hoilo : image  size reduce & quality for faster loading
+                  alt={name}
+                  width={100}
+                  height={100}
+                ></Image>
               </div>
             </div>
 
-            <h2 className="font-semibold text-lg">Emma Wilson</h2>
-
+            <h2 className="font-semibold text-lg">{name}</h2>
+            {/* status of that friens */}
             <div className="flex gap-2 mt-1">
-              <span className="badge badge-error badge-sm">Overdue</span>
-              <span className="badge badge-success badge-sm">Family</span>
+              <span
+                className={`py-1 px-3 rounded-2xl text-sm
+                 ${
+                   status === "on track"
+                     ? "bg-green-700 text-white "
+                     : status === "overdue"
+                       ? "bg-red-600"
+                       : "bg-yellow-300"
+                 }`}
+              >
+                {status}
+              </span>
             </div>
-
-            <p className="text-sm italic text-gray-500 mt-3">
-              Former colleague, great mentor
-            </p>
-
-            <p className="text-xs text-gray-400 mt-1">Preferred: email</p>
+            {/* bio of that friens */}
+            <p className="text-sm italic text-gray-500 mt-3">{bio}</p>
+            {/* preferred email of that friend */}
+            <p className="text-xs text-gray-400 mt-1">Preferred: {email}</p>
           </div>
         </div>
 
         {/* Sidebar Actions */}
         <div className="flex flex-col gap-2">
           <button className="btn  bg-base-100 border shadow-sm">
-            <MdSnooze /> Snooze 2 Weeks
+            <MdSnooze /> Snooze {Math.round(days_since_contact / 7)} week
           </button>
 
           <button className="btn  bg-base-100 border shadow-sm">
--- src/app/Components/ProfileDetailsCard/ProfileDetailsCard.jsx
+++ src/app/Components/ProfileDetailsCard/ProfileDetailsCard.jsx
@@ -1,47 +1,58 @@
 import QuickButton from "../QuickButton/QuickButton";
 import SetCard from "../SetCards/SetCard";
 
-const ProfileDetailsCard = () => {
+const ProfileDetailsCard = ({ details }) => {
+  const { days_since_contact, next_due_date, goal } = details;
+
+  // function to format date like figma
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const options = { month: "short", day: "numeric", year: "numeric" };
+    return date.toLocaleDateString("en-US", options);
+  };
+
   return (
-    <div >
+    <div>
       <div className="flex flex-col gap-6">
-          
-          {/* STATS ROW */}
-          <div className="flex flex-col sm:flex-row justify-between gap-3">
-            <SetCard  value="62" label="Days Since Contact" />
-            <SetCard value="30" label="Goal (Days)" />
-            <SetCard value="Feb 27, 2026" label="Next Due" />
-          </div>
-
-          {/* RELATIONSHIP GOAL */}
-          <div className="card bg-base-100  shadow-sm">
-            <div className="flex justify-between items-center p-5">
-              
-              <div>
-                <h3 className="mb-4 text-xl font-bold text-green-900">Relationship Goal</h3>
-                <p className="text-sm text-gray-500 mt-1">
-                  Connect every <span className="font-extrabold">30 days</span>
-                </p>
-              </div>
-
-              <button className="btn btn-sm">Edit</button>
-            </div>
-          </div>
-
-          {/* QUICK CHECK-IN */}
-          <div className="card bg-base-100  shadow">
-            <div className="p-5">
-              <h3 className="mb-4 text-2xl font-bold text-green-900">Quick Check-In</h3>
-
-              <div className="grid grid-cols-3 gap-4">
-                <QuickButton icon="📞" label="Call" />
-                <QuickButton icon="💬" label="Text" />
-                <QuickButton icon="🎥" label="Video" />
-              </div>
-            </div>
-          </div>
-
+        {/* STATS ROW */}
+        <div className="flex flex-col sm:flex-row justify-between gap-3">
+          <SetCard value={days_since_contact} label="Days Since Contact" />
+          <SetCard value={goal} label="Goal (Days)" />
+          <SetCard value={formatDate(next_due_date)} label="Next Due" />
         </div>
+
+        {/* RELATIONSHIP GOAL */}
+        <div className="card bg-base-100  shadow-sm">
+          <div className="flex justify-between items-center p-5">
+            <div>
+              <h3 className="mb-4 text-xl font-bold text-green-900">
+                Relationship Goal
+              </h3>
+              <p className="text-sm text-gray-500 mt-1">
+                Connect every{" "}
+                <span className="font-extrabold">{goal} days</span>
+              </p>
+            </div>
+
+            <button className="btn btn-sm">Edit</button>
+          </div>
+        </div>
+
+        {/* QUICK CHECK-IN */}
+        <div className="card bg-base-100  shadow">
+          <div className="p-5">
+            <h3 className="mb-4 text-2xl font-bold text-green-900">
+              Quick Check-In
+            </h3>
+
+            <div className="grid grid-cols-3 gap-4">
+              <QuickButton icon="📞" label="Call" />
+              <QuickButton icon="💬" label="Text" />
+              <QuickButton icon="🎥" label="Video" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
