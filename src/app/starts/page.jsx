830cb09bd08e7ce3f3136f2a4863912de0a780e3
Ashraf Hossain Akash <nirjhorakash07@gmail.com>
2026-04-16 20:20:22 +0600

    Added stats title

--- src/app/starts/page.jsx
+++ src/app/starts/page.jsx
@@ -1,5 +1,10 @@
 import StatsComponent from "../Components/StatsComponent/StatsComponent";
 
+//adding metadata
+export const metadata = {
+  title: "Stats Page",
+};
+
 const StartsPage = () => {
   return <StatsComponent></StatsComponent>;
 };
